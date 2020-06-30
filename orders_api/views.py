f9ee53936e478071334c6e6821effb462fc3f215
Ramiro Otermin <oterminramiro@gmail.com>
2020-06-30 18:28:44 -0300

    added jwt to item list

--- orders_api/views.py
+++ orders_api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from django.core import serializers
+
 import jwt
 import random
 
@@ -13,43 +15,32 @@ from .models import CustomerCode as CustomerCodeModel
 class CustomerList(generics.ListCreateAPIView):
 	queryset = Customer.objects.all()
 	serializer_class = CustomerSerializer
+
 # GET A SINGLE USER BY SEARCHING FOR PHONE
 class CustomerExist(generics.RetrieveAPIView):
 	queryset = Customer.objects.all()
 	serializer_class = CustomerSerializer
 	lookup_field = 'phone'
+
 # SEND CODE
 class CustomerCode(APIView):
-
 	def post(self,request):
-
 		try:
-
 			phone = request.data['phone']
 			customer = Customer.objects.filter(phone=phone).first()
-
 			if customer:
-
 				code = str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9)) + str(random.randrange(9))
 				data = {"customer": customer.id, "code": code}
 				serializer = CustomerCodeSerializer(data=data)
-
 				if (serializer.is_valid()):
-
 					serializer.save()
 					return Response('true')
-
 				else:
-
 					return Response(serializer.errors)
-
 			else:
-
 				return Response('Customer not found')
-
 		except Exception as e:
-
-			raise Exception(e)
+			return Response(str(e))
 
 # LOGIN AND JWT RESPONSE
 class CustomerLogin(APIView):
@@ -72,13 +63,27 @@ class CustomerLogin(APIView):
 			else:
 				return Response('Customer not found')
 		except Exception as e:
-			raise Exception(e)
-			#return Response('Server error')
+			return Response(str(e))
 
 # GET ALL ITEMS
 class ItemList(generics.ListAPIView):
-	queryset = Item.objects.all()
-	serializer_class = ItemSerializer
+	def get(self,request):
+
+		try:
+			token = jwt.decode(request.headers['x-auth-token'], 'secret', algorithms=['HS256'])
+		except Exception as e:
+			return Response(str(e))
+			
+		customer = Customer.objects.filter(phone=token['phone']).first()
+
+		if customer:
+			queryset = Item.objects.all()
+			serializer = ItemSerializer(queryset, many=True)
+			return Response(serializer.data)
+		else:
+			return Response('Customer not found')
+
+		return Response('server error')
 # GET A SINGLE ITEM BY PK
 class ItemExist(generics.RetrieveAPIView):
 	queryset = Item.objects.all()
