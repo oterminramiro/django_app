2a46d869cddd88f981bcb73499d07e84ce837def
Ramiro Otermin <oterminramiro@gmail.com>
2020-07-30 18:13:46 -0300

    update to docker

--- app/settings.py
+++ app/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
